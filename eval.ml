95aea33596bae41e8754af9966afc63551a6dd02
P4ttyCakes <patricklu@Patricks-MacBook-Air.local>
2025-08-05 17:11:54 -0400

    feat: implement variable lookup in evaluator

--- eval.ml
+++ eval.ml
@@ -7,12 +7,23 @@ and env = (string * value) list
 
 let rec eval_expr (env : env) (e : expr) : value =
   match e with
-  | NumLit n -> Int n
-  | Var x -> List.assoc x env
+
+  (* Numlits literals evaluate to themselves *)
+  | NumLit n ->
+      Int n
+
+  (* Look up variable in current environment *)
+  | Var x ->
+      List.assoc x env
+
+  (* Unary negation *)
   | UnOp (OpNeg, e1) ->
-      (match eval_expr env e1 with
+      let v1 = eval_expr env e1 in
+      (match v1 with
        | Int n -> Int (-n)
        | _ -> failwith "Unary negation on non-integer")
+
+  (* Binary operations *)
   | BinOp (op, e1, e2) ->
       let v1 = eval_expr env e1 in
       let v2 = eval_expr env e2 in
@@ -20,14 +31,24 @@ let rec eval_expr (env : env) (e : expr) : value =
        | (OpPlus, Int n1, Int n2) -> Int (n1 + n2)
        | (OpMinus, Int n1, Int n2) -> Int (n1 - n2)
        | (OpTimes, Int n1, Int n2) -> Int (n1 * n2)
-       | _ -> failwith "Binary op on non-integers")
+       | _ -> failwith "Binary operation on non-integers")
+
+  (* Let-binding: let x = e1 in e2 *)
   | Let (x, e1, e2) ->
-      let v1 = eval_expr env e1 in
-      eval_expr ((x, v1) :: env) e2
-  | Fun (x, body) -> Closure (x, body, env)
+      let v1 = eval_expr env e1 in                    (* Evaluate e1 *)
+      let extended_env = (x, v1) :: env in            (* Extend environment with (x, v1) *)
+      eval_expr extended_env e2                      (* Evaluate e2 in the extended env *)
+
+  (* Function definition: fun x -> body *)
+  | Fun (x, body) ->
+      Closure (x, body, env)                          (* Create closure with param x, body, and current env *)
+
+  (* Function application: e1 e2 *)
   | App (e1, e2) ->
-      match eval_expr env e1 with
-      | Closure (x, body, closure_env) ->
-          let arg_val = eval_expr env e2 in
-          eval_expr ((x, arg_val) :: closure_env) body
-      | _ -> failwith "Application on non-function"
+      let f = eval_expr env e1 in                     (* Evaluate function expression *)
+      let arg = eval_expr env e2 in                   (* Evaluate argument expression *)
+      (match f with
+       | Closure (x, body, closure_env) ->
+           let extended_env = (x, arg) :: closure_env in   (* Extend captured env with parameter binding *)
+           eval_expr extended_env body               (* Evaluate body of function in new env *)
+       | _ -> failwith "Attempted to apply a non-function")
